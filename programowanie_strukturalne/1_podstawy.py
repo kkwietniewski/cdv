b3cb252b90736678d09c2545eb81712a205ca929
Kacper Kwietniewski <kkwietniewski@edu.cdv.pl>
2019-03-25 15:58:27 +0100

    python

--- programowanie_strukturalne/1_podstawy.py
+++ programowanie_strukturalne/1_podstawy.py
@@ -1,1 +1,87 @@
 print("cdv")
+print("2")
+
+#-komentarz
+#potęga
+
+'''
+potega = 2**10
+print(potega)
+#pobieranie danych z klawatury
+imie = input("Podaj imie: ")
+#konkatenacja +
+print("Twoje imie podane z klawatury: " + imie)
+
+nazwisko = input("Podaj nazwisko: ")
+print("Twoje imie:" + imie + ", Twoje nazwisko: "+ nazwisko)
+
+dlugosc = len(nazwisko)
+#konwersja na int
+print("Dlugosc nazwiska: " + str(dlugosc))
+print("Dlugosc nazwiska: " , dlugosc)
+#typ danych np int
+print(type(dlugosc))
+'''
+#uzytkownik podaje z klawiatury swoj wiek wyswietl dane w formacie Twoj wiek: ... lat
+'''
+wiek = input("\nPodaj wiek: ")
+print("Twoj wiek: " , wiek , " lat" )
+
+
+print("\nTwoj wiek: ", end="")
+wiek = input()
+print("Twoj wiek: " , wiek , " lat" )
+'''
+#komentarz do wielu linii ''' '''
+'''
+nazwisko = 'Kowalski'
+pierwszyZnak = nazwisko[0]
+#pierwsza litera "K"
+print(pierwszyZnak)
+#poza zakresem
+#ostatniZnak = nazawisko[len(nazwisko)]
+#ostatnia litera "i"
+ostatniZnak = nazwisko[len(nazwisko)-1]
+ostatniZnak = nazwisko[-1]
+print(ostatniZnak)
+print(ostatniZnak)
+'''
+#konwersja danych
+'''
+x = "5"
+print(type(x))
+x = int(x)
+print(type(x))
+'''
+#mnozenie tesktu
+'''
+text = "Jan" * 2
+print(text)
+print(type(text))
+'''
+#dzielenie
+'''
+y = 6
+print(type(y))
+y = y / 2
+print(type(y))
+print(y)
+'''
+'''
+wiek = 21
+print("Twoj wiek:", wiek)
+
+print("Twoj wiek: " + str(wiek))
+'''
+
+'''
+nazwisko = "Jankowski"
+print(nazwisko[0])
+print(nazwisko[0:3])
+#print(nazwisko[:3])
+print(nazwisko[-2:])
+print(nazwisko[:-2])#Jankows
+print(nazwisko[:-2:2])#Jnos
+print(nazwisko[::2])#Jnosi
+
+'''
