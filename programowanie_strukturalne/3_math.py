ef5fcdebf33dfdba2b1fdfba5f1777c194e63ae3
kkwietniewski <kkwietniewski@edu.cdv.pl>
2019-05-06 10:23:54 +0200

    matma

---
+++ programowanie_strukturalne/3_math.py
@@ -0,0 +1,16 @@
+#pi
+import math
+pi = math.pi
+print(pi)
+
+#pierwiastek
+pierwsiastek = math.sqrt(9)
+print(pierwsiastek)
+
+#moduł random
+import random
+losuj = random.random()
+print(losuj)
+
+losujZlisty = random.choice([11, 20, 13, 7])
+print(losujZlisty)
